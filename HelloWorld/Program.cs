5e124368aa5b569ef25a143a69698974c86f71d9
Zeus <reeseborchersmsu@gmail.com>
2020-03-05 14:09:31 -0600

    example2

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace myApp {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World! IM GONNA TAKE THIS JOB BY FORCE, MAY THE FORCE BE WITH YOU!");
+            Console.WriteLine ("My name is Obi Wan the current time is " + DateTime.Now);
+        }
+    }
+}
